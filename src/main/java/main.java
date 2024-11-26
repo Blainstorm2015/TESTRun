a44f66ed9b58494dcd8a5a1a4a8cf46d9a47948f
Christian Klein <blainstorm2015@gmail.com>
2024-11-27 00:34:48 +0100

    First Commit

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -3,10 +3,19 @@ public class main {
         System.out.println("Hello World!");
 
         test();
+        int a = 5;
+        int b = 10;
+
+        System.out.println("The sum of " + a + " and " + b + " is " + add(a, b));
     }
+        
 
     public static void test() {
         System.out.println("Hello World!");
-
     }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
 }
