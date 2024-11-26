c100837a287c8462fc05820af6569ea8086efdc9
Christian Klein <blainstorm2015@gmail.com>
2024-11-27 00:33:53 +0100

    First Commit

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,5 +1,11 @@
 public class main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        test();
+    }
+
+    public static void test() {
+        System.out.println("Hello World!");
     }
 }
